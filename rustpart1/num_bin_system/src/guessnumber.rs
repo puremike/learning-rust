92c1632abaf1f5f0ad2fc5fbb5fa3366e56a9040
Michael Egbinola <michaelmarketing25@gmail.com>
2025-05-06 11:38:17 -0500

    created a simple guessing game and ensuring that it's within the specified number range

---
+++ rustpart1/num_bin_system/src/guessnumber.rs
@@ -0,0 +1,33 @@
+use rand::Rng;
+use std::io;
+
+pub fn guessnumber() {
+    println!("Guessing Game");
+    println!("Enter a number between 1 to 100: ");
+
+    let mut number: String = String::new();
+
+    io::stdin()
+        .read_line(&mut number)
+        .expect("Failed to read line");
+
+    // let numner: i32 = number.trim().parse().unwrap();
+
+    let number: i32 = match number.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Please enter a number in range");
+            return;
+        }
+    };
+
+    assert!(number >= 1 && number <= 100, "Number out of range");
+    println!("You guessed: {}", number);
+}
+
+pub fn guessnumberrandom() {
+    println!("Guessing Game from Random Number");
+
+    let number: i32 = rand::thread_rng().gen_range(1..=100);
+    println!("You guessed: {}", number);
+}
--- rustpart1/num_bin_system/src/main.rs
+++ rustpart1/num_bin_system/src/main.rs
@@ -1,6 +1,10 @@
+mod guessnumber;
 mod numbertypes;
 fn main() {
     println!("Number Types Examples");
 
     numbertypes::usenumbertypes();
+
+    guessnumber::guessnumber();
+    guessnumber::guessnumberrandom();
 }
